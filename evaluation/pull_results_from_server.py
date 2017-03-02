1a70f92137a49676e2535bb0c6ae9c559efd1386
Sven Eberhardt <sven2@brown.edu>
2017-03-02 14:33:02 -0500

    Force overwrite for results pulling

--- evaluation/pull_results_from_server.py
+++ evaluation/pull_results_from_server.py
@@ -5,10 +5,10 @@ import os, subprocess
 from hmax.levels import util
 from rapid_categorization import config
 
-def pull_results(experiment_run):
+def pull_results(experiment_run, force_overwrite=False):
     cifs_filename = util.get_experiment_db_filename_by_run(experiment_run)
     remote_filename = config.deploy_machine + ':' + os.path.join(config.deploy_folder_name, experiment_run, 'participants.db')
-    if not os.path.exists(cifs_filename):
+    if force_overwrite or (not os.path.exists(cifs_filename)):
         subprocess.call(['scp', remote_filename, cifs_filename])
         print 'File saved to %s' % cifs_filename
     else:
@@ -16,4 +16,4 @@ def pull_results(experiment_run):
 
 
 if __name__ == '__main__':
-    pull_results('clicktionary400msvaranswerfull')
+    pull_results('clicktionary400msvaranswerfull', force_overwrite=True)
